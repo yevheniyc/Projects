1571d2c92171408fb5e6fe8653d1e869902e43a9
whitehat <yevheniyc@gmail.com>
2016-01-20 16:24:57 -0800

    SocketIO: move wikipedia challenge response into Python repo.

---
+++ SocketIO/README.md
@@ -0,0 +1,40 @@
+Handling Wiki* edits with SocketIO and threading.
+
+NOTES:
+Code specifically processes the incomming RCStream from stream.wikimedia.org, not the hardcoded dummy data
+
+1. Default socketIO-client installs socket.io protocol 1.x, whereas RCStream runs on 0.9.
+   To fix, manually install 0.5.6:
+   >> pip install https://pypi.python.org/packages/source/s/socketIO-client/socketIO-client-0.5.6.tar.gz
+
+2. Review information:
+    - Review concurrency, threads, sockets, web sockets: 
+        - Gevent library: http://learn-gevent-socketio.readthedocs.org/en/latest/general_concepts.html
+        - Python sockets overview: https://docs.python.org/2/howto/sockets.html
+        - SocketIO-client API: https://pypi.python.org/pypi/socketIO-client/0.5.6
+    - Read and understand the provided material and suggested links
+    - Read over RCStream API
+    - Read over SocketIO API
+
+3. Design and architecture:
+    - Understand the provided code: comment
+    - Test the provided code and run it locally
+    - Implement and test process_change(wait_secs) function without threading
+    - Add multi-threading  
+
+4. Resoning behind threading:
+    - The objective is to 
+        - process the incoming RCStream data faster than implemented by each stream's processing algorithm.
+        - the order of incomming streams must stay intact during output.
+    - The provided process_change(wait_secs) function, therefore, needs to be executed concurrently,
+      so that in a specified amount of time (5 mins) more than 5 mins worth of processing could be done.
+    - To execute the processing function concurrently, multiple-threads were used
+    - To keep track of the threads order, a global THREADS list is used. Each time thread is started, it is added to the list.
+      After threads in the list have been joined, the list is emptied and thread generation continues. 
+    - After multiple tests, 10 concurrent threads were enough to significantly increase efficiency of processing data (i.e ~200 seconds worth
+      of processing was done in 60 seconds)
+
+5. Next Steps and Design Flaws:
+    - I tested thread order and efficiency of the multi-threading using a timer, could be viewd in the commits, however, I did not have
+      time to write proper tests. 
+    - The next step would be to implement the threads not only concurrently, but also in parallel using multiple processes.  
---
+++ SocketIO/record_rc.py
@@ -0,0 +1,83 @@
+
+import time
+import json
+import random
+import sys
+import threading
+from math import log
+import socketIO_client
+import stopit
+
+# Keeping thread count and order
+# - chose 10 because it accomplished the job
+# - more testing could be done with higher numbers
+THREADS_MAX = 10
+THREADS = []
+
+def process_change(wait_secs):
+    """ Sample method, could be used for processing data. 
+
+        Args:
+            wait_secs: time to mimick processing time.
+    """
+    start = time.time()
+    while time.time() - start < wait_secs:
+        time.sleep(0.001)
+
+
+class WikiNamespace(socketIO_client.BaseNamespace):
+
+    def on_change(self, change):
+        """ Triggered when json data is arriving from the stream and
+            contains a "new" or "edit" type.
+
+            Args:
+                change: json data describing a stream.
+        """
+        global THREADS_MAX, THREADS
+        # if json's type is new or edit
+        if change['type'] in ('new', 'edit'):
+            # implement with threading
+            for counter in range(THREADS_MAX):
+                # generate a different wait time for each thread
+                wait = random.lognormvariate(log(1), 1)
+                # update json's wait field
+                change['wait'] = min(max(wait, 0.01), 10)
+                # build the tread
+                thread = threading.Thread(target=process_change, 
+                                          args=(change['wait'],))
+                thread.start()
+                # record the sequence
+                THREADS.append(thread)
+            
+            # waiting for threads to finish
+            for thread in THREADS:
+                thread.join()
+                # write out each thread's output keeping json format
+                json.dump(change, sys.stdout)
+                sys.stdout.write("\n")
+
+            # Reset thread counter for the next round
+            THREADS = []
+
+    def on_connect(self):
+        """ Sends a "subscribe" message to the indicated server
+            when connection has been established
+        """
+        self.emit('subscribe', 'en.wikipedia.org')  # Subscribes to enwiki
+
+# Create a socket to the specified URL, PORT
+socketIO = socketIO_client.SocketIO("stream.wikimedia.org", 80)
+# Define a namespace for the /rc requests
+socketIO.define(WikiNamespace, '/rc')
+
+# For the next 5 minutes wait for streaming data
+with stopit.ThreadingTimeout(5 * 60) as to_ctx_mgr:
+    try:
+        # wait and receive the incoming stream
+        socketIO.wait(10000)
+    except KeyboardInterrupt:
+        sys.stderr.write("Keyboard interrupt detected.  Shutting down.\n")
+
+# Once all of the threads have been joined and connections complete/interrupted
+sys.stderr.write("All done in.\n")
