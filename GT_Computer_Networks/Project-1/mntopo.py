ac6133204a52843c08fe55f073bd70e23a5ead89
mininet <yevheniyc@gmail.com>
2016-01-24 23:01:37 -0500

    GT_CN: cleaning solution to project 1.

--- GT_Computer_Networks/Project-1/mntopo.py
+++ GT_Computer_Networks/Project-1/mntopo.py
@@ -14,16 +14,21 @@ class MNTopo(Topo):
 
         # Host and link configuration
         hostConfig = {'cpu': cpu}
-        linkConfig = {'bw': 10, 'delay': '1ms', 'loss': 0,
+        linkConfig = {'bw': 50, 'delay': '10ms', 'loss': 0,
                    'max_queue_size': max_queue_size }
 
         # Hosts and switches
         s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+	s3 = self.addSwitch('s3') 
+
         sender = self.addHost('sender', **hostConfig)
         receiver = self.addHost('receiver', **hostConfig)
 
         # Wire receiver
         self.addLink(receiver, s1, **linkConfig)
+        self.addLink(s1, s2, **linkConfig)
+	self.addLink(s2, s3, **linkConfig)
 
         # Wire sender
-        self.addLink(sender, s1, **linkConfig)
+        self.addLink(sender, s3, **linkConfig)
