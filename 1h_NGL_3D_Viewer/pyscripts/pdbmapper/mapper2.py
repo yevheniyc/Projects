7fdc19e0b19e8ac55405df8b29982b8be20f1593
whitehat <yevheniyc@gmail.com>
2017-06-07 09:12:08 -0700

    ngl: need to change the process from processing an input sequence file, to processing a sequence.

---
+++ 1h_NGL_3D_Viewer/pyscripts/pdbmapper/mapper2.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python
+import os.path
+import json
+from argparse import ArgumentParser
+from collections import OrderedDict
+
+import PdbParser as pp
+import SeqAlign as sa
+import InputProcessor2 as ip
+import FastaGen as fg
